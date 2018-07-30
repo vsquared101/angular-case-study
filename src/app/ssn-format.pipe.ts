da9f661d96ac58a6372458c74e377159e3696655
Vivek Varma <vsquared101@gmail.com>
2018-07-30 14:20:15 +0000

    Resolve compilation issue due to code in ssn format pipe.

--- src/app/ssn-format.pipe.ts
+++ src/app/ssn-format.pipe.ts
@@ -6,8 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SsnFormatPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    rawSsn: string = value;
-    return rawSsn.substring(0,3) + '-' + rawSsn.substring(3,6)  + '-' + rawSsn.substring(6,9);
   }
 
 }
