7d42c07d1b17403abd9f205d8a7739e1a3eb0241
Vivek Varma <vsquared101@gmail.com>
2018-07-31 12:59:16 +0000

    Add global styles to styles css file.

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute  } from '@angular/router';
+import { UserService } from '../user.service';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  user: any;
+  userForm: FormGroup;
+  ssnPattern: string = '^[0-9]{9}$';
+  passportPattern: string = '^[a-zA-Z]{1}[0-9]{7}$';
+
+  constructor(private service: UserService, private route: ActivatedRoute, fb: FormBuilder) { 
+  
+    this.userForm = fb.group({
+      'prefix': ['', Validators.required],
+      'firstname': ['', Validators.required],
+      'middlename': [''],
+      'lastname': ['', Validators.required],
+      'shortname': ['', Validators.required],
+      'ssn': ['', [Validators.required, Validators.pattern(this.ssnPattern)]],
+      'gender': ['', Validators.required],
+      'countryOfResidence': ['', Validators.required],
+      'passport': ['', [Validators.required, Validators.pattern(this.passportPattern)]],
+      'countryOfIssuance': ['', Validators.required],
+      'issuanceDate' : [''],
+      'expirationDate' : [''],
+      'noOfDependents' : [''],
+      'maritalStatus' : ['']
+    });
+  }
+
 
   ngOnInit() {
+      
+    this.route.params.subscribe( params => {
+      this.id = +params['id'];
+      console.log('id value passed is: ' + this.id);
+      
+      // TODO call getUserById method of the user service here to get particular user based on ID
+      // set the above user object to a local 'user' variable to enable display of the fields within it.
+      this.user = {
+          "id": 1,
+          "full_name": "Mr Charles Stewart",
+          "short_name": "Charles Stewart",
+          "ssn": "11-22-3333",
+          "dob": "10-10-1968",
+          "gender": "Male",
+          "marital_status": "Married",
+          "citizenship": "United States",
+          "country_of_residence": "Australia",
+          "passport": "H1234567",
+          "country_of_issuance": "Australia",
+          "issuance_date": "15-04-2010",
+          "expiration_date": "15-04-2020",
+          "no_of_dependents": 2
+      };
+    });
+    
+  }
+  
+  updateUserRecord(data) {
+    console.log(data);
   }
 
 }
--- src/app/view-user/view-user.component.ts
+++ src/app/view-user/view-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService  } from '../user.service';
+import { ActivatedRoute  } from '@angular/router';
 
 @Component({
   selector: 'app-view-user',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-user.component.css']
 })
 export class ViewUserComponent implements OnInit {
+  id: number;
+  user: any;
 
-  constructor() { }
+  constructor(private service: UserService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+      
+    this.route.params.subscribe( params => {
+      this.id = +params['id'];
+      console.log('id value passed is: ' + this.id);
+      
+      // TODO call getUserById method of the user service here to get particular user based on ID
+      // set the above user object to a local 'user' variable to enable display of the fields within it.
+      this.user = {
+          "id": 1,
+          "full_name": "Mr Charles Stewart",
+          "short_name": "Charles Stewart",
+          "ssn": "11-22-3333",
+          "dob": "10-10-1968",
+          "gender": "Male",
+          "marital_status": "Married",
+          "citizenship": "United States",
+          "country_of_residence": "Australia",
+          "passport": "H1234567",
+          "country_of_issuance": "Australia",
+          "issuance_date": "15-04-2010",
+          "expiration_date": "15-04-2020",
+          "no_of_dependents": 2
+      };
+    });
+    
   }
 
 }
