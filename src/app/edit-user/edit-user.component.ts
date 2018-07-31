8412e79362aa99b564e9835b73bc99c8818daf8a
Vivek Varma <vsquared101@gmail.com>
2018-08-01 00:17:06 +0530

    Correct user object structure to match UI fields.

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -36,33 +36,16 @@ export class EditUserComponent implements OnInit {
     });
   }
 
-
   ngOnInit() {
-      
     this.route.params.subscribe( params => {
       this.id = +params['id'];
       console.log('id value passed is: ' + this.id);
       
-      // TODO call getUserById method of the user service here to get particular user based on ID
-      // set the above user object to a local 'user' variable to enable display of the fields within it.
-      this.user = {
-          "id": 1,
-          "full_name": "Mr Charles Stewart",
-          "short_name": "Charles Stewart",
-          "ssn": "111222333",
-          "dob": "10-10-1968",
-          "gender": "Male",
-          "marital_status": "Married",
-          "citizenship": "United States",
-          "country_of_residence": "Australia",
-          "passport": "H1234567",
-          "country_of_issuance": "Australia",
-          "issuance_date": "15-04-2010",
-          "expiration_date": "15-04-2020",
-          "no_of_dependents": 2
-      };
+      this.service.getUserById(this.id)
+        .subscribe((data) => {
+          this.user = data;
+        });
     });
-    
   }
   
   updateUserRecord(data) {
--- src/app/view-user/view-user.component.ts
+++ src/app/view-user/view-user.component.ts
@@ -19,24 +19,10 @@ export class ViewUserComponent implements OnInit {
       this.id = +params['id'];
       console.log('id value passed is: ' + this.id);
       
-      // TODO call getUserById method of the user service here to get particular user based on ID
-      // set the above user object to a local 'user' variable to enable display of the fields within it.
-      this.user = {
-          "id": 1,
-          "full_name": "Mr Charles Stewart",
-          "short_name": "Charles Stewart",
-          "ssn": "111222333",
-          "dob": "10-10-1968",
-          "gender": "Male",
-          "marital_status": "Married",
-          "citizenship": "United States",
-          "country_of_residence": "Australia",
-          "passport": "H1234567",
-          "country_of_issuance": "Australia",
-          "issuance_date": "15-04-2010",
-          "expiration_date": "15-04-2020",
-          "no_of_dependents": 2
-      };
+      this.service.getUserById(this.id)
+        .subscribe((data) => {
+          this.user = data;
+        });
     });
     
   }
